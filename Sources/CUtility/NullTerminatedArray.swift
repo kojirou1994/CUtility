f539562483848a24a4d8938f4670525addbc961e
kojirou <kojirouhtc@gmail.com>
2022-05-05 18:33:57 +0800

    add NullKeyPathTerminatedArray

--- Sources/CUtility/NullTerminatedArray.swift
+++ Sources/CUtility/NullTerminatedArray.swift
@@ -6,13 +6,34 @@ public struct NullTerminatedArray<T>: Sequence, IteratorProtocol {
     current = pointer
   }
 
-  @inline(__always)
   public mutating func next() -> UnsafePointer<T>? {
     if _slowPath(current == nil) {
       return nil
     }
     defer {
       current = current?.successor()
+    }
+    return current
+  }
+
+}
+
+public struct NullKeyPathTerminatedArray<T, R>: Sequence, IteratorProtocol {
+
+  private var current: UnsafePointer<T>
+  private let keypath: KeyPath<T, R?>
+
+  public init(_ pointer: UnsafePointer<T>, keypath: KeyPath<T, R?>) {
+    current = pointer
+    self.keypath = keypath
+  }
+
+  public mutating func next() -> UnsafePointer<T>? {
+    if _slowPath(current.pointee[keyPath: keypath] == nil) {
+      return nil
+    }
+    defer {
+      current = current.successor()
     }
     return current
   }
