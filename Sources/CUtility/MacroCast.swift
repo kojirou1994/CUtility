cb31b5f4c92a48063d8dff8b7b9dae66ad90a723
kojirou <kojirouhtc@gmail.com>
2022-07-24 04:19:43 +0800

    macroCast: use generic macroValue

--- Sources/CUtility/MacroCast.swift
+++ Sources/CUtility/MacroCast.swift
@@ -1,4 +1,14 @@
 @inlinable @inline(__always)
-public func macroCast<U>(_ macroValue: Int32) -> U where U: BinaryInteger {
-  .init(truncatingIfNeeded: UInt32(bitPattern: macroValue))
+public func macroCast<T, U>(_ macroValue: T) -> U where T: FixedWidthInteger, U: BinaryInteger {
+  let bitsContainer: UInt64
+  if macroValue.signum() == -1 {
+    let extraBitCount = UInt64.bitWidth - T.bitWidth
+    bitsContainer = (UInt64(truncatingIfNeeded: macroValue) << extraBitCount) >> extraBitCount
+  } else {
+    bitsContainer = numericCast(macroValue)
+  }
+
+  let result = U(truncatingIfNeeded: bitsContainer)
+
+  return result
 }
