9d5947bda9aeb15810fcf9dcbc0aef2969cd087f
kojirou <kojirouhtc@gmail.com>
2022-10-27 11:26:23 +0800

    create LazyCopiedCString from StaticCString

--- Sources/CUtility/LazyCopiedCString.swift
+++ Sources/CUtility/LazyCopiedCString.swift
@@ -1,25 +1,34 @@
 public final class LazyCopiedCString {
 
+  @inlinable
   public init(cString: UnsafePointer<CChar>, forceLength: Int? = nil, freeWhenDone: Bool) {
     self.cString = cString
     self.forceLength = forceLength
     self.freeWhenDone = freeWhenDone
   }
 
+  @inlinable
+  public convenience init(_ cString: StaticCString) {
+    self.init(cString: cString.cString, freeWhenDone: false)
+  }
+
   public let cString: UnsafePointer<CChar>
-  private let forceLength: Int?
-  private let freeWhenDone: Bool
+  @usableFromInline
+  internal let forceLength: Int?
+  @usableFromInline
+  internal let freeWhenDone: Bool
 
   /// lazy and cached length
-  public internal(set) lazy var length: Int = {
+  public private(set) lazy var length: Int = {
     forceLength ?? UTF8._nullCodeUnitOffset(in: cString)
   }()
 
   /// lazy-copied native string
-  public internal(set) lazy var string = {
+  public private(set) lazy var string = {
     String(decoding: UnsafeRawBufferPointer(start: cString, count: length), as: UTF8.self)
   }()
 
+  @inlinable
   deinit {
     if freeWhenDone {
       cString.deallocate()
