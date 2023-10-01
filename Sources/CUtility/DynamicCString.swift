6f4ad357bf2fff883cd860c866f266d960a19502
kojirou <kojirouhtc@gmail.com>
2023-10-02 01:34:36 +0800

    DynamicCString should be mutable

--- Sources/CUtility/DynamicCString.swift
+++ Sources/CUtility/DynamicCString.swift
@@ -1,15 +1,15 @@
 public struct DynamicCString: ~Copyable {
 
   @inlinable
-  public init(cString: consuming UnsafePointer<CChar>) {
+  public init(cString: consuming UnsafeMutablePointer<CChar>) {
     self.cString = cString
   }
 
   @usableFromInline
-  internal let cString: UnsafePointer<CChar>
+  internal let cString: UnsafeMutablePointer<CChar>
 
   @inlinable
-  public consuming func take() -> UnsafePointer<CChar> {
+  public consuming func take() -> UnsafeMutablePointer<CChar> {
     let v = cString
     discard self
     return v
