c6b0a5978a1fa4577a968a9beb1dc610edfc75d7
kojirou <kojirouhtc@gmail.com>
2023-12-16 07:01:14 +0800

    add macro CGeneric

--- Package.swift
+++ Package.swift
@@ -1,13 +1,28 @@
 // swift-tools-version: 5.9
 
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
   name: "CUtility",
+  platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
   products: [
+    .library(name: "CGeneric", targets: ["CGeneric"]),
     .library(name: "CUtility", targets: ["CUtility"]),
   ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+  ],
   targets: [
+    .macro(
+      name: "CUtilityMacros",
+      dependencies: [
+        .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+        .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+      ]
+    ),
+    .target(name: "CGeneric", dependencies: ["CUtilityMacros"]),
+
     .target(name: "CUtility"),
     .target(name: "CTestCode"),
     .testTarget(
---
+++ Sources/CGeneric/CGeneric.swift
@@ -0,0 +1,2 @@
+@attached(peer, names: overloaded)
+public macro CStringGeneric() = #externalMacro(module: "CUtilityMacros", type: "CStringGenericMacro")
