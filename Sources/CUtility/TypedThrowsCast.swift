d2c9e5c09fe60b124cc2e3eecfb100b835739eee
kojirou <kojirouhtc@gmail.com>
2024-09-23 00:51:29 +0800

    toTypedThrows+Noncopyable

--- Sources/CUtility/TypedThrowsCast.swift
+++ Sources/CUtility/TypedThrowsCast.swift
@@ -1,6 +1,6 @@
 @_alwaysEmitIntoClient
 @inlinable @inline(__always)
-public func toTypedThrows<Result, E: Error>(_ error: E.Type, _ body: () throws(any Error) -> Result) throws(E) -> Result {
+public func toTypedThrows<Result: ~Copyable, E: Error>(_ error: E.Type, _ body: () throws(any Error) -> Result) throws(E) -> Result {
   do {
     return try body()
   } catch let e as E {
