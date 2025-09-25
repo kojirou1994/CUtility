96c72cf8ce5c1d6a5137d77f0bd06d939e2b23ba
kojirou <kojirouhtc@gmail.com>
2025-09-25 16:31:35 +0800

    StaticString is CStringConvertible

--- Sources/CUtility/CStringConvertible.swift
+++ Sources/CUtility/CStringConvertible.swift
@@ -16,6 +16,34 @@ extension String: CStringConvertible {
   }
 }
 
+// use with
+// typealias StringLiteralType = StaticString
+extension StaticString: CStringConvertible {
+  @_alwaysEmitIntoClient
+  @inlinable @inline(__always)
+  public func withUnsafeCString<R, E>(_ body: (UnsafePointer<CChar>) throws(E) -> R) throws(E) -> R where E : Error, R : ~Copyable {
+    if hasPointerRepresentation {
+      try body(UnsafeRawPointer(utf8Start).assumingMemoryBound(to: CChar.self))
+    } else {
+      // copy to stack!
+      try safeInitialize { (result: inout Result<R, E>?) in
+        withUTF8Buffer { utf8 in
+          result = .init { () throws(E) -> R in
+            try toTypedThrows(E.self) {
+              try withUnsafeTemporaryAllocation(of: UInt8.self, capacity: utf8.count + 1) { string in
+                _ = string.initialize(from: utf8)
+                string[utf8.count] = 0
+
+                return try body(UnsafeRawPointer(string.baseAddress!).assumingMemoryBound(to: CChar.self))
+              }
+            }
+          }
+        }
+      }.get()
+    }
+  }
+}
+
 extension UnsafeRawPointer: CStringConvertible {
   @_alwaysEmitIntoClient
   @inlinable @inline(__always)
