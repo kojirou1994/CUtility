94491f5c4b01f7614f891017689d8af1505c7bd8
kojirou <kojirouhtc@gmail.com>
2025-09-28 10:30:54 +0800

    RawSpan is ContiguousUTF8Bytes

--- Sources/CUtility/ContiguousUTF8.swift
+++ Sources/CUtility/ContiguousUTF8.swift
@@ -99,3 +99,11 @@ extension EmptyCollection: ContiguousUTF8Bytes where Element == UInt8 {
     try body(.init(start: nil, count: 0))
   }
 }
+
+extension RawSpan: ContiguousUTF8Bytes {
+  @_alwaysEmitIntoClient
+  @inlinable @inline(__always)
+  public func withContiguousUTF8Bytes<R, E>(_ body: (UnsafeRawBufferPointer) throws(E) -> R) throws(E) -> R where E : Error, R : ~Copyable {
+    try withUnsafeBytes(body)
+  }
+}
