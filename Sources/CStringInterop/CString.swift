2ea745fc20554438b72f010b95b2ff2694f897eb
kojirou <kojirouhtc@gmail.com>
2025-12-07 14:42:24 +0800

    update

--- Sources/CStringInterop/CString.swift
+++ Sources/CStringInterop/CString.swift
@@ -2,7 +2,7 @@ public typealias CString = CStringConvertible & ~Copyable & ~Escapable
 
 extension DefaultStringInterpolation {
   @inlinable
-  public mutating func appendInterpolation(_ cString: borrowing some CString) {
+  public mutating func appendInterpolation(cString: borrowing some CString) {
     appendInterpolation(cString.withUnsafeCString(String.init(cString: )))
   }
 }
